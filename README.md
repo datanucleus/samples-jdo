267b34d8b498144550e509a01ef7fc3acce8069d
datanucleus <andy@datanucleus.org>
2013-07-31 19:01:34 +0100

    in-mem variant

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 sample-jdoql-method
 ===================
 
-Sample showing how to add support for a Java method to JDOQL
+Sample showing how to add support for a Java method to JDOQL for RDBMS,
+and an evaluator for use when evaluating the query in-memory.
+
+Look at
+
+* <a href="https://github.com/datanucleus/sample-jdoql-method/blob/master/src/main/resources/META-INF/MANIFEST.MF">src/main/resources/META-INF/MANIFEST.MF</a>   The MANIFEST file for the plugin jar, defining the plugin imports/exports.
+* <a href="https://github.com/datanucleus/sample-jdoql-method/blob/master/src/main/resources/plugin.xml">src/main/resources/plugin.xml</a>   The plugin.xml file for the plugin jar, defining what features are being provided by this plugin.
+
+Provides a very simple test of the use of this JDOQL method.
---
+++ src/main/java/mydomain/datanucleus/LongValueOfInMemoryEvaluator.java
@@ -0,0 +1,78 @@
+/**********************************************************************
+Copyright (c) 2013 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
+package org.datanucleus.query.evaluator.memory;
+
+import org.datanucleus.exceptions.NucleusException;
+import org.datanucleus.query.QueryUtils;
+import org.datanucleus.query.expression.InvokeExpression;
+import org.datanucleus.query.expression.Literal;
+import org.datanucleus.query.expression.ParameterExpression;
+import org.datanucleus.query.expression.PrimaryExpression;
+
+/**
+ * Evaluator for the method "Long.valueOf({stringExpr})".
+ */
+public class LongValueOfInMemoryEvaluator implements InvocationEvaluator
+{
+    /* (non-Javadoc)
+     * @see org.datanucleus.query.evaluator.memory.InvocationEvaluator#evaluate(org.datanucleus.query.expression.InvokeExpression, org.datanucleus.query.evaluator.memory.InMemoryExpressionEvaluator)
+     */
+    public Object evaluate(InvokeExpression expr, Object invokedValue, InMemoryExpressionEvaluator eval)
+    {
+        String method = expr.getOperation();
+
+        if (invokedValue != null)
+        {
+            throw new NucleusException("Attempt to invoke Long.valueOf on " + invokedValue + " but this is a static method!");
+        }
+
+        // Evaluate the first argument
+        String arg1 = null;
+        Object arg1Obj = null;
+        Object param = expr.getArguments().get(0);
+        if (param instanceof PrimaryExpression)
+        {
+            PrimaryExpression primExpr = (PrimaryExpression)param;
+            arg1Obj = eval.getValueForPrimaryExpression(primExpr);
+        }
+        else if (param instanceof ParameterExpression)
+        {
+            ParameterExpression paramExpr = (ParameterExpression)param;
+            arg1Obj = QueryUtils.getValueForParameterExpression(eval.getParameterValues(), paramExpr);
+        }
+        else if (param instanceof Literal)
+        {
+            arg1Obj = ((Literal)param).getLiteral();
+        }
+        else if (param instanceof InvokeExpression)
+        {
+            arg1Obj = eval.getValueForInvokeExpression((InvokeExpression) param);
+        }
+        else
+        {
+            throw new NucleusException("Long.valueOf(...) requires arg of type String, but was " + param + " - unsupported");
+        }
+        arg1 = QueryUtils.getStringValue(arg1Obj);
+        if (!(arg1 instanceof String))
+        {
+            throw new NucleusException("Long.valueOf(...) requires arg of type String, but was " + arg1 + " - unsupported");
+        }
+
+        return Long.valueOf(arg1);
+    }
+}
