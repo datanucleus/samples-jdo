c582e1e697b1badbcbb9f02c106990031b6a833a
Andy Jefferson <andy@datanucleus.org>
2017-01-08 08:14:06 +0000

    Update MyTenancyProvider.java
    javadocs

--- multitenant/src/test/java/org/datanucleus/test/MyTenancyProvider.java
+++ multitenant/src/test/java/org/datanucleus/test/MyTenancyProvider.java
@@ -3,6 +3,10 @@ package org.datanucleus.test;
 import org.datanucleus.store.schema.MultiTenancyProvider;
 import org.datanucleus.ExecutionContext;
 
+/**
+ * Example of a provider of tenant "id".
+ * To use this, you should specify the persistence property "datanucleus.TenantProvider" and set to an instance of this.
+ */
 public class MyTenancyProvider implements MultiTenancyProvider
 {
     public String getTenantId(ExecutionContext ec)
