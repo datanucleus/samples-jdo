63f2a08ad61bc73d76745717c7428a7b89ca16fe
andyjefferson <andy@datanucleus.org>
2018-04-26 14:58:30 +0100

    use "distance" in the query example

--- geospatial/src/main/java/org/datanucleus/samples/jdo/spatial/Main.java
+++ geospatial/src/main/java/org/datanucleus/samples/jdo/spatial/Main.java
@@ -37,7 +37,8 @@ public class Main
             sps[1] = new Position("rent-a-car", new Point("SRID=4326;POINT(10 0)"));
             sps[2] = new Position("pizza shop", new Point("SRID=4326;POINT(20 0)"));
 
-            Position home = new Position("home", new Point("SRID=4326;POINT(0 0)"));
+            Point homepoint = new Point("SRID=4326;POINT(0 0)");
+            Position home = new Position("home", homepoint);
 
             System.out.println("Persisting spatial data...");
             System.out.println(home);
@@ -51,18 +52,22 @@ public class Main
 
             tx.commit();
  
-            //query for the distance
+            // Query for all Positions within a distance
             tx.begin();
 
-            System.out.println("Retrieving position where X position is > 10 and Y position is 0 ... Found:");
+            Double distance = new Double(12.0);
+            System.out.println("Retrieving Positions where distance to home is less than \"" + distance + "\" ... Found:");
 
             // TODO Make this more elaborate, demonstrating more of the power of spatial method querying
-            Query query = pm.newQuery(Position.class, "name != 'home' && (this.point.getX() > 10) && (this.point.getY() == 0)");
-            List list = (List) query.execute();
-            for( int i=0; i<list.size(); i++)
+            Query query = pm.newQuery(Position.class, "name != 'home' && Spatial.distance(this.point, :homepoint) < :distance");
+            List list = (List) query.execute(homepoint, distance);
+            for (int i=0; i<list.size(); i++)
             {
                 System.out.println(list.get(i));
             }
+
+//          Query query = pm.newQuery(Position.class, "name != 'home' && (this.point.getX() > 10) && (this.point.getY() == 0)");
+//          List list = (List) query.execute();
 
             //clean up database.. just for fun :)
             pm.newQuery(Position.class).deletePersistentAll();
